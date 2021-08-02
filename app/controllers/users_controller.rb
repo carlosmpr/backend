084ab4cbf3ed9007fee6d4bbf0daf94304efcc51
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-08-02 11:07:16 -0400

    Adding user info route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ require 'jwt'
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
+  def me
+    user  = User.validateUser(params[:token])
+    render json: user
+  end
+
   def create
     hmac_secret = 'my$ecretK3y'
     @user = User.new(user_params)
     if @user.save
       token = JWT.encode user_model(@user), hmac_secret, 'HS256'
-      render json: {msg: token}
+      render json: {token: token}
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -30,7 +35,7 @@ class UsersController < ApplicationController
       @user = User.find_by(email: params[:email])
       if @user.authenticate(params[:password])
         token = JWT.encode user_model(@user), hmac_secret, 'HS256'
-        render json: {msg: token}
+        render json: {token: token}
       else
         render json: {msg:'Invalid credentials'}
       end
